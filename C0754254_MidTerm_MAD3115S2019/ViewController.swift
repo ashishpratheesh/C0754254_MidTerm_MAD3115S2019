14ec183196bb30bc556e6cb25349b9faf15556f5
Ashish pratheeesh <ashishpradeesh@gmail.com>
2019-07-12 01:34:54 -0400

    [ashish]added background image on login

--- C0754254_MidTerm_MAD3115S2019/ViewController.swift
+++ C0754254_MidTerm_MAD3115S2019/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var idtext: UITextField!
+    @IBOutlet weak var passwordtxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+
         // Do any additional setup after loading the view.
     }
 
