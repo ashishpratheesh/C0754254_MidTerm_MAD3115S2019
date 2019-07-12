79c398abdf1a2d971172b5b2df114b97376bf5ed
Ashish pratheeesh <ashishpradeesh@gmail.com>
2019-07-12 18:45:11 -0400

    [ashish] added email validation

--- C0754254_MidTerm_MAD3115S2019/ViewController.swift
+++ C0754254_MidTerm_MAD3115S2019/ViewController.swift
@@ -21,48 +21,7 @@ class ViewController: UIViewController {
         backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
         self.view.insertSubview(backgroundImage, at: 0)
         
-        
-}
-    @IBAction func BtnLoginClick(_ sender: Any)
-    {
-        if let email = txtemailId.text{
-                if !email.isEmpty{
-              if email.isValidEmail(){
-                if let password = txtPassword.text{
-                            if !password.isEmpty{
-                                
-                                if (password.count < 6)
-                                {
-                                    showAlert(msg: "Size of password must be equal or greater than 6")
-                                }else{
-                                    if isValidUser(email: email, password: password) {
-                                        setRememberMe()
-                                        
-                                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                                        let dashboardVC = storyboard.instantiateViewController(withIdentifier: "homeVC") as! CustomersListViewController
-                                        
-                                        self.navigationController?.pushViewController(dashboardVC, animated: true)
-                                        
-                                        
-                                    }else{
-                                        showAlert(msg: "You have entered wrong credentials")
-                                    }
-                                }
-                                
-                            }else{
-                                showAlert(msg: "Please Enter Password")
-                            }
-                        }
-                    }
-                    else{
-                        showAlert(msg: "Please Enter Valid Email")
-                    }
-                }else{
-                    showAlert(msg: "Please Enter Email")
-                }
-            }
-        }
     }
 }
-
+   // @IBAction func BtnLoginClick(_ sender: Any)
 
